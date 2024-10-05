347124018061384a457eeb62d1a211fe2fb12679
martynasd123 <dargis.martynas@gmail.com>
2024-10-05 20:32:30 +0200

    Anteater patroling/chasing states

--- anteater/anteater.gd
+++ anteater/anteater.gd
@@ -4,9 +4,16 @@ extends CharacterBody2D
 @export var path: NodePath
 
 @onready var _anim = $AnimatedSprite2D
+@onready var _sniff_area: Area2D = $"Sniff area"
+
+enum { STATE_CHASING, STATE_PATROLLING }
 
 var patrol_points
 var patrol_index = 0
+
+var target: Node2D
+
+var state = STATE_PATROLLING
 
 const move_speed = 100.0
 
@@ -19,12 +26,35 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _physics_process(delta: float):
+	
+func patrol(delta: float) -> void:
 	var target = patrol_points[patrol_index]
 	if position.distance_to(target) < 1:
 		patrol_index = wrapi(patrol_index + 1, 0, patrol_points.size())
 		target = patrol_points[patrol_index]
 	velocity = (target - position).normalized() * move_speed
+	
+func chase(delta: float) -> void:
+	velocity = (target.position - position).normalized() * move_speed
+
+func _physics_process(delta: float):
+	if state == STATE_PATROLLING:
+		patrol(delta)
+	else:
+		chase(delta)
+		
 	rotation = velocity.angle() + deg_to_rad(90)
 	move_and_slide()
+
+
+func _on_sniff_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("ants"):
+		target = body
+		state = STATE_CHASING
+
+
+func _on_sniff_area_body_exited(body: Node2D) -> void:
+	for b in _sniff_area.get_overlapping_bodies():
+		if b.is_in_group("ants"):
+			return
+	state = STATE_PATROLLING
