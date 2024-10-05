21311f6df16532b028cf21864dbf408669ed68ae
martynasd123 <dargis.martynas@gmail.com>
2024-10-05 16:47:18 +0200

    Zoom out and folow ants

--- game_scene.gd
+++ game_scene.gd
@@ -4,6 +4,7 @@ extends Node2D
 @export var ant_scene: PackedScene
 
 @onready var _ants_parent = $Ants
+@onready var _cam = $Camera2D
 
 func _ready() -> void:
 	for sugar in get_tree().get_nodes_in_group("sugars"):
@@ -11,6 +12,7 @@ func _ready() -> void:
 
 func _on_sugar_picked_up() -> void:
 	var ant_count = _ants_parent.get_child_count()
+	_cam.zoom *=0.9
 	for i in range(ant_count):
 		var new_ant = ant_scene.instantiate()
 		new_ant.position = _ants_parent.get_child(i).position
@@ -19,4 +21,10 @@ func _on_sugar_picked_up() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var avg_pos = Vector2(0, 0)
+	var ant_count = _ants_parent.get_child_count()
+	for i in range(ant_count):
+		avg_pos += _ants_parent.get_child(i).position
+	if ant_count > 0:
+		avg_pos /= ant_count
+	_cam.position = avg_pos
